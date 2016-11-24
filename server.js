ee859b1b2f69dc0a7a538f7a7faa85300af6784f
Pedro Teixeira <pedro.ap.teixeira@gmail.com>
2016-11-24 16:28:27 +0000

    Added projects page

--- server.js
+++ server.js
@@ -41,6 +41,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects', {
+    title: 'Projects'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({ errorMessage: 'Unable to fulfill this request' });
 });
